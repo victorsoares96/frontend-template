c3bcdc5fc17ee4aa4f6a04147ea14a4e6fb375b6
Victor Soares <vitorsoares96@hotmail.com>
2022-10-13 12:26:16 -0300

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,9 @@ export default defineConfig(() => {
         },
       }),
     ],
+    define: {
+      'process.env': {},
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
